67fc24585b06d5bc25e7e9f600a7fc90aaf403b2
fyayla <furkanyayla2002@gmail.com>
2022-06-21 01:30:51 +0600

    fixed the line issue

--- src/component/todos.js
+++ src/component/todos.js
@@ -19,8 +19,8 @@ const Todos = ({ todos, deleteTodo }) => {
             AddTodo.js. All we need to do is return the todo list item {todo.content} */}
             <ListItemButton component="a" href="#simple-list">
               <Checkbox style={{ paddingLeft: 0 }} color="primary" onClick={() => deleteTodo(todo.id)} />
-              <ListItemText primary={todo.content} />
-              <ListItemText secondary={todo.date} />
+              <ListItemText primary={todo.content} secondary={todo.date} />
+               
             </ListItemButton>
           </Card>
         </Grid>
